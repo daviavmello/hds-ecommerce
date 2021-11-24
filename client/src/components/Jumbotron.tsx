1e92bd4ef0361f1ead1a6a7be6d643d37dbcee4b
Davi Mello <davimello1994@gmail.com>
2021-11-24 12:11:42 -0500

    favicon

--- client/src/components/Jumbotron.tsx
+++ client/src/components/Jumbotron.tsx
@@ -3,7 +3,7 @@ import { Button } from "./Button";
 
 export const Jumbotron: React.FC = () => (
   <div className="h-screen-90 bg-homeImg bg-cover rounded-xl mx-auto text-center flex flex-col justify-center">
-    <h2 className="text-3xl leading-9 font-bold tracking-tight text-tertiary sm:text-4xl sm:leading-10 ">
+    <h2 className="text-3xl leading-9 font-bold tracking-tight text-gray-800 sm:text-4xl sm:leading-10 ">
       Bringing that at home feeling.
     </h2>
     <h4 className="my-4">Explore new design solutions for your home today.</h4>
