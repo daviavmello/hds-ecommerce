73dd105719af402a83e309da1863b711917170e7
Davi Mello <davimello1994@gmail.com>
2021-11-24 16:04:00 -0500

    updating api endpoint

--- client/src/api/storeHelper.ts
+++ client/src/api/storeHelper.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getVendors = async () => {
   try {
-    const response = await axios.get(`http://www.localhost:3001/vendors`);
+    const response = await axios.get(`https://hds-ecommerce.herokuapp.com/vendors`);
     return response.data;
   } catch (error: any) {
     return error.response;
@@ -11,7 +11,7 @@ export const getVendors = async () => {
 
 export const getProducts = async () => {
   try {
-    const response = await axios.get(`http://www.localhost:3001/products`);
+    const response = await axios.get(`https://hds-ecommerce.herokuapp.com/products`);
     return response.data;
   } catch (error: any) {
     return error.response;
