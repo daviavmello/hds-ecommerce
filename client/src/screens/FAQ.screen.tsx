2aca49bb01e3fc4ee0f40deaef77395e96630370
Davi Mello <davimello1994@gmail.com>
2021-11-30 12:11:56 -0500

    faq update

--- client/src/screens/FAQ.screen.tsx
+++ client/src/screens/FAQ.screen.tsx
@@ -6,69 +6,153 @@ export const FAQ: React.FC = () => (
     <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 pt-4 md:px-8 px-2">
       <div>
         <h4 className="font-semibold text-accent">
-          What is included with a remodeling estimate?
+          How much will my cabinets cost?
         </h4>
         <p className="text-justify">
-          Our free remodeling estimates include a detailed written scope of
-          work. At times, when necessary or when requested, we will provide
-          renderings for you to view what your remodel will look like upon
-          completion. Renderings look like a picture of your home and are a
-          likeness of what your remodel will look like so you can visualize the
-          space to be remodeled.
+          Cabinets vary dramatically in price just like cars or houses.
+          Entry-level cabinets are less expensive than cabinets with special
+          finishes and upgraded storage features. HDS provides cabinetry that
+          fits most any budget—from basic to bold.
         </p>
       </div>
       <div>
         <h4 className="font-semibold text-accent">
-          Will I need to pay for a design agreement?
+          How long will it take to get the cabinets I order?
         </h4>
         <p className="text-justify">
-          Most kitchen and bathroom remodels are contracted without a design
-          agreement. Design agreements are most typically used for major
-          renovations of multiple rooms (such as a whole house remodel) or home
-          addition projects.
+          Order time varies depending on the manufacturer and the complexity of
+          the job. The minimum time is roughly three weeks and can be as much as
+          eight weeks during peak times of the year.
         </p>
       </div>
       <div>
         <h4 className="font-semibold text-accent">
-          What is the difference between an estimate and a design agreement?
+          Can you design my kitchen from a sketch of the room that I provide?
         </h4>
         <p className="text-justify">
-          An estimate is free and is an educated price that is prepared for your
-          project. A design agreement means you chose to select Classic Home
-          Improvements as your builder but you are not quite sure yet on the
-          design you want to build.
+          We can certainly start with a sketch you provide. Your forethought in
+          doing some homework can shorten the time needed to design the final
+          layout. However, we will always do our own measurements to verify that
+          the design will actually work.
         </p>
       </div>
       <div>
         <h4 className="font-semibold text-accent">
-          How long will my project take?
+          What is the best type of countertop for the kitchen?
         </h4>
         <p className="text-justify">
-          During our free consultation with you, we will discuss the duration of
-          your project. The key element in completing your project in a timely
-          manner is for you to be available during the process so you can select
-          your products.
-        </p>
-      </div>
-      <div>
-        <h4 className="font-semibold text-accent">Do you use subcontractors?</h4>
-        <p className="text-justify">
-          We employ both subcontractors and hourly employees who are on our
-          payroll. The subcontractors we work with have all worked with us for
-          several years and for most of them, we are their sole source of
-          employment because we are able to keep them busy from one job to the
-          next.
+          There are many possible countertop choices. As with cabinets, your
+          budget needs to be matched with your desire for flexibility of design
+          and product longevity. Laminate, solid surface, quartz-enhanced and
+          granite are all good choices.
         </p>
       </div>
       <div>
         <h4 className="font-semibold text-accent">
-          Will there be a big mess during our project?
+          Can you match my existing cabinets or furniture?
         </h4>
         <p className="text-justify">
-          Construction is messy. It is dirty and dusty. Before it is beautiful,
-          it is a construction zone. However, we respect your property by laying
-          down clean tarps as needed and cleaning the work space and traffic
-          areas daily.
+          All woods and stains change color over time—some more so than others.
+          In many cases, HDS can find cabinetry that may be compatible with your
+          existing cabinets or furniture. In no case can we guarantee an exact
+          match.
+        </p>
+      </div>
+      <div>
+        <h4 className="font-semibold text-accent">
+          What construction features should I look for when choosing cabinets?
+        </h4>
+        <p className="text-justify">
+          Certain construction features are a sign of quality cabinets. A savvy
+          consumer will look for things such as finished backs in all cabinets,
+          drawer guides that also support the drawer bottom, conversion varnish
+          (not lacquer) finishes, multi-way adjustable hinges, adjustable
+          shelves, and a wide range of heights and depths. Newer convenience
+          features include “soft-close” hinges and drawers, multi-function
+          drawer systems, and optional task lighting. <br />
+          Be sure to check for the KCMA (Kitchen Cabinet Manufacturers
+          Association) label, a certification that the cabinets meet
+          construction standards designed to ensure many years of trouble-free
+          service.
+        </p>
+      </div>
+      <div>
+        <h4 className="font-semibold text-accent">
+          What are the most popular types of cabinets?
+        </h4>
+        <p className="text-justify">
+          While popular cabinet styles and trends vary from year to year,
+          traditional raised-panel oak styling is always a favorite due to
+          affordability and durability. A current trend is a “stand-alone”
+          furniture look that allows for highly customized design features using
+          cabinetry from major manufacturers. <br />
+          Besides oak, certain wood species tend to be favored by today's
+          consumers. Maple is frequently used in remodeling because of its clean
+          look and its ability to blend in with most existing finishes. Cherry
+          has become increasingly popular as manufacturers have developed a
+          variety of finishes allowing it to work in more applications.
+        </p>
+      </div>
+      <div>
+        <h4 className="font-semibold text-accent">
+          Are environmentally “green” cabinets available at a reasonable price?
+        </h4>
+        <p className="text-justify">
+          HDS represents several manufacturers across the price spectrum whose
+          cabinets are certified under the Environmental Stewardship Program
+          developed by the KCMA (Kitchen Cabinet Manufacturers Association).
+        </p>
+      </div>
+      <div>
+        <h4 className="font-semibold text-accent">
+          What's the difference between manufactured cabinets and custom-made
+          cabinets?
+        </h4>
+        <p className="text-justify">
+          Years ago there was a significant difference in the sizes and finishes
+          available from "custom" cabinetmakers and the "stock" manufacturers.
+          Today, many cabinet manufacturers offer custom sizing and finishes
+          that rival, and may even exceed, the possibilities of a local custom
+          cabinet shop.
+        </p>
+      </div>
+      <div>
+        <h4 className="font-semibold text-accent">
+          Do you charge a design fee, measure fee or other fees in addition to
+          the contract price for cabinets and installation?
+        </h4>
+        <p className="text-justify">
+          HDS does not charge a measure fee or a design fee for a first design
+          or estimate. In some cases, where multiple design revisions are
+          requested, a design retainer may be requested. That retainer is then
+          credited in full to the final cost of the job.
+        </p>
+      </div>
+      <div>
+        <h4 className="font-semibold text-accent">
+          Do you do major remodeling?
+        </h4>
+        <p className="text-justify">
+          HDS specializes in "light" kitchen, office, or laundry room
+          remodeling. We have strong working relationships with several quality,
+          full-line remodeling firms for those jobs that require major
+          structural renovation.
+        </p>
+      </div>
+      <div>
+        <h4 className="font-semibold text-accent">
+          Do you provide cabinets for remodeling only, or also for new
+          construction?
+        </h4>
+        <p className="text-justify">
+          HDS works with a number of reputable remodelers and can provide
+          cabinets through your contractor or directly to you for your kitchen
+          or bath remodeling project or room addition. We also work with many
+          custom home builders, who consult with our designers to assure that
+          the layouts for kitchen cabinets, bath cabinets, laundry room cabinets
+          and other cabinetry are chosen to properly fit the budget and space.
+          We will coordinate directly with your builder and also consult
+          directly with you
         </p>
       </div>
     </div>
